596ca7eb8c4f3a7733bb8184349f9edd19aca389
egconley <31393715+egconley@users.noreply.github.com>
2020-01-15 11:18:10 -0800

    finished feature tasks for lab 06 - still need to write tests

--- inheritance/src/main/java/inheritance/Review.java
+++ inheritance/src/main/java/inheritance/Review.java
@@ -1,17 +1,24 @@
 package inheritance;
 
+import java.util.ArrayList;
+
 public class Review {
     String reviewAuthor;
+    String reviewedRestaurant;
     String reviewBody;
     int reviewStarScore;
+    ArrayList<Review> allReviews = new ArrayList<>();
 
-    public Review(String reviewAuthor, String reviewBody, int reviewStarScore) {
+    public Review(String reviewAuthor, String reviewedRestaurant, String reviewBody, int reviewStarScore) {
         this.reviewAuthor = reviewAuthor;
+        this.reviewedRestaurant = reviewedRestaurant;
         this.reviewBody = reviewBody;
         this.reviewStarScore = reviewStarScore;
     }
 
     public String toString() {
-        return reviewAuthor + " gave" + " restaurant " + reviewStarScore + " stars: " + reviewBody;
+        return reviewAuthor + " gave " + reviewedRestaurant + " " + reviewStarScore + " stars: " + reviewBody;
     }
+
+
 }
--- inheritance/src/test/java/inheritance/RestaurantTest.java
+++ inheritance/src/test/java/inheritance/RestaurantTest.java
@@ -11,20 +11,34 @@ Restaurant test;
 
     @Before
     public void setUp() throws Exception {
-        Restaurant test = new Restaurant("Bob's", 3, "$$");
+        Restaurant test = new Restaurant("Bob's", 3, 2);
     }
 
     @Test
     public void test_Restaurant_constructor() {
-        Restaurant test = new Restaurant("Bob's", 3, "$$");
+        Restaurant test = new Restaurant("Bob's", 3, 2);
         assertEquals("Bob's", test.restaurantName);
         assertEquals(3, test.starScore);
-        assertEquals("$$", test.priceCategory);
+        assertEquals(2, test.priceCategory);
     }
 
     @Test
     public void test_Restaurant_toString() {
-        Restaurant test = new Restaurant("Bob's", 3, "$$");
-        assertEquals("Bob's | 3 | $$", test.toString());
+        Restaurant test = new Restaurant("Bob's", 3, 2);
+        assertEquals("Bob's | 3 | 2", test.toString());
+    }
+
+    @Test
+    public void test_Restaurant_addReview() {
+        Restaurant test = new Restaurant("Bob's", 3, 2);
+        System.out.println("test = " + test);
+
+        test.addReview(new Review("Ellen", "Bob's", "Bob's is good.", 3));
+        test.addReview(new Review("Anna", "Bob's", "Bob's is gross.", 1));
+
+        System.out.println("test = " + test);
+
+        //System.out.println(test.restaurantReviews);
+        //assertEquals("[Ellen gave Bob's 3 stars: Bob's is good., Anna gave Bob's 1 stars: Bob's is gross.]", test.restaurantReviews);
     }
 }
